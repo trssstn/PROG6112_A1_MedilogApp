e3f26012b012e26c06800603c33521aba9bca05f
ST10467193_Tristan_Kriel_BCA1_PROG5112_GR02 <krieltristan68@gmail.com>
2025-09-04 17:26:23 +0200

    Added admission status class

---
+++ src/medilog/AdmissionStatus.java
@@ -0,0 +1,40 @@
+package medilog;
+
+import java.util.Date;
+
+class AdmissionStatus {
+    private boolean isAdmitted;
+    private String ward;
+    private String department;
+    private String bedNumber;
+    private Date admissionDate;
+    private Date dischargeDate;
+    private String dischargeStatus;
+    
+    public AdmissionStatus() {
+        this.isAdmitted = false;
+    }
+    
+    public void admitPatient(String ward, String department, String bedNumber) {
+        this.isAdmitted = true;
+        this.ward = ward;
+        this.department = department;
+        this.bedNumber = bedNumber;
+        this.admissionDate = new Date();
+    }
+    
+    public void dischargePatient(String status) {
+        this.isAdmitted = false;
+        this.dischargeDate = new Date();
+        this.dischargeStatus = status;
+    }
+    
+    //Getters
+    public boolean isAdmitted() { return isAdmitted; }
+    public String getWard() { return ward; }
+    public String getDepartment() { return department; }
+    public String getBedNumber() { return bedNumber; }
+    public Date getAdmissionDate() { return admissionDate; }
+    public Date getDischargeDate() { return dischargeDate; }
+    public String getDischargeStatus() { return dischargeStatus; }
+}
