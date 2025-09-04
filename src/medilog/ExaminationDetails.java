e44df4803b3f809f93287af153958a941889873a
ST10467193_Tristan_Kriel_BCA1_PROG5112_GR02 <krieltristan68@gmail.com>
2025-09-04 17:13:48 +0200

    Expanded examination details class

--- src/medilog/ExaminationDetails.java
+++ src/medilog/ExaminationDetails.java
@@ -18,5 +18,42 @@ class ExaminationDetails {
         this.examinationDate = new Date();
     }
     
+    //Getters and Setters
+    public String getPatientHistory() { return patientHistory; }
+    public void setPatientHistory(String patientHistory) {
+        this.patientHistory = patientHistory;
+    }
     
+    public String getPresentingComplaint() { return presentingComplaint; }
+    public void setPresentingComplaint(String presentingComplaint) {
+        this.presentingComplaint = presentingComplaint;
+    }
+    
+    public String getSignsAndSymptoms() { return signsAndSymptoms; }
+    public void setSignsAndSymptoms(String signsAndSymptoms) {
+        this.signsAndSymptoms = signsAndSymptoms;
+    }
+    
+    public void addPhysicalExamination(String bodySystem, String findings, int index) {
+        if (index >= 0 && index < physicalExamination.length) {
+            physicalExamination[index] = bodySystem + ": " + findings;
+        }
+    }
+    
+    public String[] getPhysicalExamination() { return physicalExamination; }
+    
+    public void addDifferentialDiagnosis(String diagnosis, int index) {
+        if (index >= 0 && index < differentialDiagnoses.length) {
+            differentialDiagnoses[index] = diagnosis;
+        }
+    }
+    
+    public String[] getDifferentialDiagnoses() { return differentialDiagnoses; }
+    
+    public String getFinalDiagnosis() { return finalDiagnosis; }
+    public void setFinalDiagnosis(String finalDiagnosis) {
+        this.finalDiagnosis = finalDiagnosis;
+    }
+    
+    public Date getExaminationDate() { return examinationDate; }
 }
